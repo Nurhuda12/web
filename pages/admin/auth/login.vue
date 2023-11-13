64ba4065934e361040a3ab84cbf5b4b9629ddf93
nurhuda <nur.huda121001@gmail.com>
2023-11-14 00:15:18 +0700

    tolong

--- pages/admin/auth/login.vue
+++ pages/admin/auth/login.vue
@@ -27,18 +27,11 @@ const isPasswordVisible = ref(false);
           </template>
 
           <VCardTitle class="font-weight-semibold text-2xl text-uppercase">
-            Materio
+            Login page
           </VCardTitle>
         </VCardItem>
 
-        <VCardText class="pt-2">
-          <h5 class="text-h5 font-weight-semibold mb-1">
-            Welcome to Materio! 👋🏻
-          </h5>
-          <p class="mb-0">
-            Please sign-in to your account and start the adventure
-          </p>
-        </VCardText>
+        
 
         <VCardText>
           <VForm @submit.prevent="() => {}">
@@ -62,17 +55,7 @@ const isPasswordVisible = ref(false);
                   @click:append-inner="isPasswordVisible = !isPasswordVisible"
                 />
 
-                <!-- remember me checkbox -->
-                <div
-                  class="d-flex align-center justify-space-between flex-wrap mt-1 mb-4"
-                >
-                  <VCheckbox v-model="form.remember" label="Remember me" />
-
-                  <a class="ms-2 mb-1" href="javascript:void(0)">
-                    Forgot Password?
-                  </a>
-                </div>
-
+                
                 <!-- login button -->
                 <VBtn block type="submit" to="/"> Login </VBtn>
               </VCol>
@@ -85,11 +68,7 @@ const isPasswordVisible = ref(false);
                 </RouterLink>
               </VCol>
 
-              <VCol cols="12" class="d-flex align-center">
-                <VDivider />
-                <span class="mx-4">or</span>
-                <VDivider />
-              </VCol>
+              
 
               <!-- auth providers -->
               <VCol cols="12" class="text-center">
